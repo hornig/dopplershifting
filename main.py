ed8004e16231e353a865f41ed0beb76f59dd6c87
hornig <>
2020-05-16 17:09:02 +0200

    initial
    speed test to methods

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import time
+
+adc_offset = -127.5
+offset = 44
+filename = "SDRSharp_20190521_191501Z_137500000Hz_IQ.wav"
+
+data = np.memmap(filename, offset=offset)
+
+time_a = time.time()
+iq = []
+for i in range(0, len(data), 2):
+    iq.append(data[i+0] + 1j*data[i+1])
+time_b = time.time()
+print(time_b - time_a)
+
+
+del iq
+
+
+
+time_a = time.time()
+window = 2048000
+
+for slice in range(0, int(len(data) // (window * 2)) * window * 2, window * 2):  # the -1 is because the slice could be less than the region we need to cut out for the out. xxx
+    data_slice = adc_offset + (data[slice: slice + window * 2: 2]) + 1j * (adc_offset + data[slice + 1: slice + window * 2: 2])
+    print(slice, len(data)/2, slice / (len(data)), len(data_slice))
+
+time_b = time.time()
+print(time_b - time_a)
