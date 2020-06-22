51be63e10954fad508ce145ae6addd31593e4c39
Nikhil Mishra <mishras.nikhil@gmail.com>
2020-06-22 23:40:38 +0530

    Cleaned up code

--- tool.py
+++ tool.py
@@ -5,79 +5,140 @@ import time
 from scipy import signal
 from scipy.fftpack import fft, fftshift, ifft
 
-offset = 44
-file1 = "../station1_yagi_SDRSharp_20170312_060959Z_137650kHz_IQ.wav"
-file2 = "../station2_turnstile_SDRSharp_20170312_061008Z_137650000Hz_IQ.wav"
-file3 = "../SDRSharp_20190521_191501Z_137500000Hz_IQ.wav" #big file
-data = np.memmap(file1, offset=offset)
+class Waterfall():
+    """Waterfall Tool Main Class"""
 
-adc_offset = -127.5
-window = 2048000
-fs = 2048000
-T = 1/fs
-total_duration = T*len(data)
-chunk = 2048000
-num_chunks = int(len(data)/(chunk*2))
-iterate = 0
-specx = []
+    def __init__(self):
+        self.specx = []
 
-def calFFT(sig):
-    norm_fft = fftshift(fft(sig))
-    abs_fft = np.abs(norm_fft)
-    return abs_fft
+    def calFFT(self, sig):
+        norm_fft = fftshift(fft(sig))
+        abs_fft = np.abs(norm_fft)
+        return abs_fft
 
-def calFFTPower(afft, fs):
-    transform = 10 * np.log10(afft)
-    fc = 0
-    freq = np.arange((-1 * fs) / 2 + fc, fs / 2 + fc)
-    return transform, freq
+    def calFFTPower(self, afft, fs):
+        transform = 10 * np.log10(afft)
+        fc = 0
+        freq = np.arange((-1 * fs) / 2 + fc, fs / 2 + fc)
+        return transform, freq
 
-time_a = time.time()
-for slice in range(0, int(len(data) // (window * 2)) * window * 2, window*2): 
-    data_slice = adc_offset + (data[slice: slice + window * 2: 2]) + 1j * (adc_offset + data[slice + 1: slice + window * 2: 2])
+    def run(self, filename, save_flag, fs=2048000):
+        """Loads the data and performs fft in chunks."""
 
-    fft_iq = calFFT(data_slice)
-    
-    transform, freq = calFFTPower(fft_iq, fs)
-    
-    if slice==0:
-        #pre storing array in mem. 
-        specx = np.zeros([num_chunks, 2048000])
+        self.filename = filename
+        self.fs = fs
+        self.save_flag = save_flag
+        offset = 44
+        data = np.memmap(filename, offset=offset)
 
-    # specx[:,iterate] = transform 
-    specx[num_chunks-iterate-1] = transform
+        T = 1/fs
+        iterate = 0 
+        adc_offset = -127.5
+        window = 2048000
+        chunk = window
+        self.total_duration = T*len(data)
+        num_chunks = int(len(data)/(chunk*2))
 
-    iterate +=1
-    print(len(fft_iq), len(transform), len(specx), iterate)
-    del data_slice,transform,fft_iq   
-   
-del data
-time_b = time.time()
-print('Time:',time_b - time_a)
-print('Len',len(specx), ' Type:', type(specx))
-print('Frequency Resolution: {} Hz'.format(fs/(2*fs)))
-#np.save('iq_save.npy', iq)
+        try:
+            self.specx = np.load("iq_spec.npy")
+            skip=True
+        except:
+            skip=False
 
-#Plotting Waterfall
-plt.figure(figsize=(20,8))
-plt.imshow(specx, extent=[(-1 * fs) / 2 + 0, (fs / 2) + 0, 0, total_duration/2], origin='lower', aspect='auto')
-plt.colorbar()
-plt.xlabel('Frequency (kHz)')
-plt.ylabel('Time (s)')
-plt.title('Waterfall - Fs=2048kHz : NOAA Sat Pass')
-plt.show()
+        if(skip == False):
+            data_slice = []
 
-#Select Chanenl:
-fc = 1.05e6 
-BW = 16e3
-start_band = fc - (BW/2)
-stop_band = fc + (BW/2)
+            time_a = time.time()
+            for slice in range(0, int(len(data) // (window * 2)) * window * 2, window*2): 
+                data_slice = adc_offset + (data[slice: slice + window * 2: 2]) + 1j * (adc_offset + data[slice + 1: slice + window * 2: 2])
 
-plt.figure(figsize=(20,8))
-plt.imshow(specx, origin='lower', aspect='auto')
-plt.colorbar()
-plt.xlabel('Frequency (kHz)')
-plt.ylabel('Time (s)')
-plt.xlim([start_band, stop_band])
-plt.title('Waterfall - Fs=2048kHz Fc = {}Hz : NOAA Sat Pass'.format(fc))
-plt.show()
+                fft_iq = self.calFFT(data_slice)
+                
+                transform, freq = self.calFFTPower(fft_iq, self.fs)
+                
+                if slice==0:
+                    #pre storing array in mem. 
+                    self.specx = np.zeros([num_chunks, 2048000])
+
+                self.specx[num_chunks-iterate-1] = transform
+
+                iterate +=1
+                print(len(fft_iq), len(transform), len(self.specx), iterate)
+                del data_slice,transform,fft_iq   
+            
+            del data
+            time_b = time.time()
+
+            if save_flag==True:
+                np.save('iq_spec.npy', self.specx)
+
+            print('Time:',time_b - time_a)
+
+        print('Len',len(self.specx), ' Type:', type(self.specx))
+        print('Frequency Resolution: {} Hz'.format(fs/(2*fs)))
+        print('Time Resolution: {} s'.format((2*fs)/fs))
+
+    def plot(self):
+        """Plots the full waterfall"""
+
+        plt.figure(figsize=(20,8))
+        plt.imshow(self.specx, extent=[(-1 * self.fs) / 2 + 0, (self.fs / 2) + 0, 0, self.total_duration/2], origin='lower', aspect='auto')
+        plt.colorbar()
+        plt.xlabel('Frequency (kHz)')
+        plt.ylabel('Time (s)')
+        plt.title('Waterfall - Fs=2048kHz : NOAA Sat Pass')
+        plt.show()
+
+    def select_channels(self, fc, BW=16e3):
+        """Plots multiple channels in on figure"""
+
+        self.BW = BW
+        self.fc = fc
+        fig, ax = plt.subplots(ncols=len(fc)) 
+        fig.suptitle('Waterfall Channel View', fontsize=15)
+        # fig.set_size_inches(20,8)
+        fig.tight_layout(pad=3)
+        start_band = []
+        stop_band = []
+
+        for n in range(len(fc)):
+            start_band.append(self.fc[n] - (self.BW/2))
+            stop_band.append(self.fc[n] + (self.BW/2))
+            # left_limit = (self.fs/2) + self.fc[n] - (self.BW/2)
+            # right_limit = (self.fs/2) - self.fc[n] + (self.BW/2)
+            ax[n].imshow(self.specx, origin='lower', aspect='auto')
+            ax[n].axvline(self.fc[n])
+            ax[n].set_xlabel('Frequency (kHz)')
+            ax[n].set_ylabel('Time (s)')
+            ax[n].set_xlim([start_band[n], stop_band[n]])
+            ax[n].set_title('Fs=2048kHz Fc={}Hz, BW = {}kHz'.format(self.fc[n], self.BW/1e3))
+            ax[n].annotate('Fc',xy=(self.fc[n], 0))
+        plt.show()
+
+    def find_signal(self):
+        #Find periodicity of the signal using zero crossing.
+        zero_crosses = np.nonzero(np.diff(self.specx > self.specx.shape[0]/2))
+        plt.hist(zero_crosses)
+        plt.xlabel('Samples')
+        plt.ylabel('Zero Crossings')
+        plt.show()
+
+def args():
+    parser = argparse.ArgumentParser(description='Reading File')
+    parser.add_argument('--f', help="Specify path to IQ .wav file", type=str)
+    parser.add_argument('--save', help="Set this flag to let program store FFT_iq file", action="store_true")
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    print("Waterfall Tool")
+
+    args_input = args()
+
+    w = Waterfall()
+    w.run(args_input.f,args_input.save)
+    # w.find_signal()
+    w.plot()
+    w.select_channels([1.05e6, 1.32e6], BW=16e3)    
+
+
